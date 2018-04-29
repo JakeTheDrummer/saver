c75bd026e752ac09b98a95d5a7c8ac3d1bfd60df
Jake Evans <evans634@msn.com>
2018-04-29 18:23:25 +0100

    Updated the API such that we are now expecting appropriate route parameters in the put, post and delete methods. These will require a datamodel to be bound

--- Saver/Saver.Server/Controllers/GoalsController.cs
+++ Saver/Saver.Server/Controllers/GoalsController.cs
@@ -11,11 +11,11 @@ namespace Saver.Server.Controllers
     /// Provides the controller endpoints for all Goals
     /// </summary>
     [RoutePrefix("api")]
-    [Route("users/{userId:int}/goals")]
+    [Route("users/{userId:int:min(1)}/goals")]
     //[Authorize]
     public class GoalsController : ApiController
     {
-        // GET api/user/{userID}/goals
+        // GET api/users/{userID}/goals
         /// <summary>
         /// Returns the collection of goals for the user
         /// with the given ID
@@ -27,27 +27,61 @@ namespace Saver.Server.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/user/{userID}/goals/{id}
+        // GET api/users/{userID}/goals/{id}
+        /// <summary>
+        /// Returns a single goal for the user
+        /// with the given ID
+        /// </summary>
+        /// <param name="userID">The ID of the User</param>
+        /// <param name="id">The ID of the goal</param>
+        /// <returns>The goal with the matching User ID and Goal ID</returns>
         [HttpGet]
-        [Route("{id:int}")]
+        [Route("{id:int:min(1)}")]
         public string Get(int userID, int id)
         {
             return "value";
         }
 
-        // POST api/values
-        public void Post([FromBody]string value)
+        // POST api/users/{userID}/goals/{id}
+        /// <summary>
+        /// Allows the user to create a goal for their account
+        /// </summary>
+        /// <param name="userId">The ID of the user for the goal</param>
+        /// <param name="goal">The goal the user wishes to create</param>
+        [HttpPost]
+        public string Post(int userId, [FromBody]string goal)
         {
+            throw new NotImplementedException();
         }
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        // PUT api/users/1/goals/5
+        /// <summary>
+        /// Allows the user to update the
+        /// </summary>
+        /// <param name="userId">The ID of the user for the goal</param>
+        /// <param name="id">The ID of the goal we wish to update</param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("{id:int:min(1)}")]
+        public string Put(int userId, int id, [FromBody]string value)
         {
+            throw new NotImplementedException();
         }
 
-        // DELETE api/values/5
-        public void Delete(int id)
+        // DELETE api/users/1/goals/5
+        /// <summary>
+        /// Allows a user to delete the goals and milestones
+        /// with the given user ID and Goal ID. This should
+        /// deallocate any funds and create transactions as
+        /// required
+        /// </summary>
+        /// <param name="userId">The ID of the User of the goal</param>
+        /// <param name="id">The ID of the goal we wish to delete</param>
+        [Route("{id:int:min(1)}")]
+        public void Delete(int userId, int id)
         {
+            throw new NotImplementedException();
         }
     }
 }
